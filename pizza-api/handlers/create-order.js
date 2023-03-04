ddc9acc6cc72221c7b15c4ed50ebb0a1aa7833bb
TCotton <me@andywalpole.me>
2023-03-04 16:45:36 +0000

    first pizza-api dynamodb call

--- pizza-api/handlers/create-order.js
+++ pizza-api/handlers/create-order.js
@@ -1,8 +1,27 @@
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 function createOrder(order) {
     const o = order;
-    if( !o || !o.id || !o.address) {
+    if (!o || !o.id || !o.address) {
         throw new Error('To order pizza, please provide pizza type and address where pizza should be delivered');
     }
-    return {}
+    return dynamoDb.put({
+        TableName: 'pizza-orders',
+        Item: {
+            orderId: 'some-id',
+            pizza: o.pizza,
+            address: o.address,
+            orderStatus: 'pending'
+        }
+    }).promise()
+        .then((res) => {
+            console.log('Order created successfully', res);
+            return res;
+        }).catch((err) => {
+            console.log('Error while creating order', err);
+            throw err;
+        });
 }
+
 module.exports = createOrder;
