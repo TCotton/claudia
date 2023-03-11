bc038b3d978f3fd251d738dc42ce3316ac11aa92
TCotton <me@andywalpole.me>
2023-03-11 14:59:51 +0000

    deleted order

--- pizza-api/handlers/delete-order.js
+++ pizza-api/handlers/delete-order.js
@@ -1,7 +1,21 @@
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
 function deleteOrder(pizzaId) {
     if( !pizzaId ) {
         throw new Error('To delete pizza details, please provide pizza type and address where pizza should be delivered');
     }
-    return {}
+    return documentClient.delete({
+        TableName: 'pizza-orders',
+        Key: {
+            orderId: pizzaId
+        }
+    }).promise().then((res) => {
+        console.log('Delete successful', res);
+        return res;
+    }).catch((err) => {
+        console.log('Error while deleting', err);
+        throw err;
+    });
 }
 module.exports = deleteOrder;
