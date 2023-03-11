9fb3cfd2d639f3795ffda8ebe1bc587a8bf91e56
TCotton <me@andywalpole.me>
2023-03-11 15:06:41 +0000

    updated order

--- pizza-api/handlers/update-order.js
+++ pizza-api/handlers/update-order.js
@@ -1,8 +1,22 @@
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
 function updateOrder(order) {
-    const o = order;
-    if( !o || !o.id || !o.address) {
+    if( !order || !order.pizzaId || !order.address) {
         throw new Error('To update pizza details, please provide pizza type and address where pizza should be delivered');
     }
-    return {}
+    return documentClient.update({
+        TableName: 'pizza-orders',
+        Key: {
+            orderId: order.pizzaId,
+            address: order.address
+        }
+    }).promise().then((res) => {
+        console.log('Update successful', res);
+        return res;
+    }).catch((err) => {
+        console.log('Error while updating', err);
+        throw err;
+    });
 }
 module.exports = updateOrder;
