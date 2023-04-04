28a8d0ea5cefdf17b33951b038941ec37d105127
TCotton <me@andywalpole.me>
2023-04-04 17:49:39 +0100

    updated claudia

--- pizza-api/handlers/create-order.js
+++ pizza-api/handlers/create-order.js
@@ -2,14 +2,23 @@ const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const rp = require('minimal-request-promise');
 
-function createOrder(order) {
-    const o = order;
-    console.log('Save an order', o);
-    if (!o || !o.pizza || !o.address) {
+function createOrder(request) {
+
+    console.log('Save an order', request.body);
+
+    const userData = request.context.authorizer.claims;
+    console.log('User data', userData);
+
+    let useraddress = request.body && request.body.address;
+    if(!useraddress) {
+        useraddress = JSON.parse(userData.address);
+    }
+
+    if (!request.body || !request.body.pizza || !request.body.address) {
         throw new Error('To order pizza, please provide pizza type and address where pizza should be delivered');
     }
 
-    return rp.post('https://some-like-it-hot-api.effortless-serverless.com/delivery', {
+    return rp.post('https://fake-delivery-api.effortlessserverless.com/delivery', {
         headers: {
             "Authorisation": "aunt-marias-pizzeria-1234567890",
             "Content-Type": "application/json"
@@ -17,7 +26,7 @@ function createOrder(order) {
         body: JSON.stringify({
             pickupTime: '15.30pm',
             pickupAddress: 'The Shire',
-            deliveryAddress: o.address,
+            deliveryAddress: useraddress,
             webhookUrl: 'https://t5xvknar88.execute-api.us-east-1.amazonaws.com/latest/orders/'
         })
     }).then(rawResponse => JSON.parse(rawResponse.body))
@@ -26,8 +35,8 @@ function createOrder(order) {
                 TableName: 'pizza-orders',
                 Item: {
                     orderId: response.deliveryId,
-                    pizza: o.pizza,
-                    address: o.address,
+                    pizza: request.body.pizza,
+                    address: useraddress,
                     orderStatus: 'pending'
                 }
             }).promise()
--- pizza-api/index.js
+++ pizza-api/index.js
@@ -9,25 +9,33 @@ const updateDeliveryStatus = require('./handlers/update-delivery-status')
 
 const api = new App()
 
+api.registerAuthorizer('userAuthentication', {
+    providerARNs: ['arn:aws:cognito-idp:us-east-1:347216043440:userpool/us-east-1_3oMwgotgr']
+});
+
 api.get('/pizzas', () => 'Hello from Pizza API')
 api.get('/orders/{id}', (request) => getPizzas(request.pathParams.id), {
     error: 404
 });
 api.post('/orders', (request) => createOrder(request.body), {
     success: 201,
-    error: 400
+    error: 400,
+    cognitoAuthorizer: 'userAuthentication'
 });
 api.put('/orders/{id}', (request) => updateOrder(request.pathParams.id, request.body), {
     success: 200,
-    error: 400
+    error: 400,
+    cognitoAuthorizer: 'userAuthentication'
 });
 api.delete('/orders/{id}', (request) => deleteOrder(request.pathParams.id), {
     success: 200,
-    error: 400
+    error: 400,
+    cognitoAuthorizer: 'userAuthentication'
 });
 api.post('/delivery', (request) => updateDeliveryStatus(request.body), {
     success: 200,
-    error: 400
+    error: 400,
+    cognitoAuthorizer: 'userAuthentication'
 });
 
 module.exports = api
