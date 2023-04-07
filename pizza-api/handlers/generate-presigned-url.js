b01dd16e3600c4ebbdfac243eb9a0b1f074af518
TCotton <me@andywalpole.me>
2023-04-07 16:46:41 +0100

    added s3 bucket handler

---
+++ pizza-api/handlers/generate-presigned-url.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const uuidv4 = require('uuid/v4');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+function generatePresignedUrl() {
+    const s3Params = {
+        Bucket: 'aunt-marias-pizzeria-testing-this-bucket',
+        Key: uuidv4(),
+        Expires: 120,
+        ACL: 'public-read'
+    };
+
+    s3.getSignedUrl('putObject', s3Params).promise()
+        .then((url) => {
+            return {
+                url: url
+            }
+        })
+}
