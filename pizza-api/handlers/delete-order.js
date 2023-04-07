7c15b45cc529811227168c56905e4da04b9e3e27
TCotton <me@andywalpole.me>
2023-04-07 16:05:37 +0100

    updated delete order to use amazon cognito

--- pizza-api/handlers/delete-order.js
+++ pizza-api/handlers/delete-order.js
@@ -1,21 +1,40 @@
 const AWS = require('aws-sdk');
 const documentClient = new AWS.DynamoDB.DocumentClient();
+const rp = require('minimal-request-promise');
 
-function deleteOrder(pizzaId) {
-    if( !pizzaId ) {
-        throw new Error('To delete pizza details, please provide pizza type and address where pizza should be delivered');
-    }
-    return documentClient.delete({
+function deleteOrder(pizzaId, userData) {
+
+    return documentClient.get({
         TableName: 'pizza-orders',
         Key: {
             orderId: pizzaId
         }
-    }).promise().then((res) => {
-        console.log('Delete successful', res);
-        return res;
-    }).catch((err) => {
-        console.log('Error while deleting', err);
-        throw err;
-    });
+    }).promise()
+        .then(result => result.Item)
+        .then(item => {
+            if (item.cognitoUsername !== userData['congiito:username']) {
+                throw new Error('You are not authorized to delete this order');
+            }
+
+            if (item.orderStatus !== 'pending') {
+                throw new Error('You can only delete pending orders');
+            }
+
+            return rp.delete(`https://fake-delivery-api.effortlessserverless.com/delivery/${orderId}`, {
+                headers: {
+                    Authorisation: 'aunt-marias-pizzeria-1234567890',
+                    'Content-Type': 'application/json'
+                }
+            })
+        }).then(() => {
+            return documentClient.delete({
+                TableName: 'pizza-orders',
+                Key: {
+                    orderId: pizzaId
+                }
+            }).promise();
+        })
+
 }
+
 module.exports = deleteOrder;
